f3bc1f1d8205fc1c74a0d4f56820706d9612299b
wxn0brP <toja.filiper@gmail.com>
2026-02-13 17:54:50 +0100

    feat: update core

--- src/actions.ts
+++ src/actions.ts
@@ -1,17 +1,12 @@
 import { CustomFileCpu } from "@wxn0brp/db-core";
-import dbActionBase from "@wxn0brp/db-core/base/actions";
-import { addId } from "@wxn0brp/db-core/helpers/addId";
-import Data from "@wxn0brp/db-core/types/data";
-import FileCpu from "@wxn0brp/db-core/types/fileCpu";
+import { CustomActionsBase } from "@wxn0brp/db-core/base/custom";
 import { DbOpts } from "@wxn0brp/db-core/types/options";
 import { VQuery } from "@wxn0brp/db-core/types/query";
-import { findUtil } from "@wxn0brp/db-core/utils/action";
 import { BinManager } from "./bin";
 
-export class BinFileAction extends dbActionBase {
+export class BinFileAction extends CustomActionsBase {
     folder: string;
     options: DbOpts;
-    fileCpu: FileCpu;
 
     /**
      * Creates a new instance of dbActionC.
@@ -51,66 +46,6 @@ export class BinFileAction extends dbActionBase {
     async issetCollection({ collection }: VQuery) {
         const collections = await this.getCollections();
         return collections.includes(collection);
-    }
-
-    /**
-     * Add a new entry to the specified database.
-     */
-    async add(query: VQuery) {
-        await this.ensureCollection(arguments[0]);
-        await addId(query, this);
-        const { collection, data } = query;
-        await this.fileCpu.add(collection, data);
-        return data;
-    }
-
-    /**
-     * Find entries in the specified database based on search criteria.
-     */
-    async find(query: VQuery) {
-        await this.ensureCollection(query);
-        return await findUtil(query, this.fileCpu, [query.collection]);
-    }
-
-    /**
-     * Find the first matching entry in the specified database based on search criteria.
-     */
-    async findOne({ collection, search, context = {}, findOpts = {} }: VQuery) {
-        await this.ensureCollection(arguments[0]);
-        let data = await this.fileCpu.findOne(collection, search, context, findOpts) as Data;
-        return data || null;
-    }
-
-    /**
-     * Update entries in the specified database based on search criteria and an updater function or object.
-     */
-    async update({ collection, search, updater, context = {} }: VQuery) {
-        await this.ensureCollection(arguments[0]);
-        return await this.fileCpu.update(collection, false, search, updater, context);
-    }
-
-    /**
-     * Update the first matching entry in the specified database based on search criteria and an updater function or object.
-     */
-    async updateOne({ collection, search, updater, context = {} }: VQuery) {
-        await this.ensureCollection(arguments[0]);
-        return await this.fileCpu.update(collection, true, search, updater, context);
-    }
-
-    /**
-     * Remove entries from the specified database based on search criteria.
-     */
-    async remove({ collection, search, context = {} }: VQuery) {
-        await this.ensureCollection(arguments[0]);
-        return await this.fileCpu.remove(collection, false, search, context);
-    }
-
-    /**
-     * Remove the first matching entry from the specified database based on search criteria.
-     */
-    async removeOne({ collection, search, context = {} }: VQuery) {
-        await this.ensureCollection(arguments[0]);
-        return await this.fileCpu.remove(collection, true, search, context);
     }
 
     /**
