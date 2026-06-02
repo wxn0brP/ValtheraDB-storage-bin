0269f2bec177d7473803c6aae14010c809031146
wxn0brP <toja.filiper@gmail.com>
2026-06-03 00:04:47 +0200

    feat: improve optimize

--- src/bin/optimize.ts
+++ src/bin/optimize.ts
@@ -1,46 +1,92 @@
-import { unlink } from "fs/promises";
+import { rename, rm } from "fs/promises";
 import { BinManager } from ".";
+import { ensureCollection } from "./collection";
 import { saveHeaderAndPayload } from "./head";
-import { HEADER_SIZE, INT_SIZE } from "./static";
-import { readData, roundUpCapacity, writeData } from "./utils";
+import { INT_SIZE } from "./static";
+import { readCollectionEof, readData, writeData } from "./utils";
 import { _log } from "../log";
+
+function isRemovedRecord(data: Buffer) {
+    return new Uint8Array(data).every(byte => byte === 0);
+}
+
+async function appendRawRecord(target: BinManager, collectionName: string, recordLengthBuffer: Buffer, data: Buffer) {
+    const collection = await ensureCollection(target, collectionName, data.length, true);
+    const collectionEOF = await readCollectionEof(target.fd, collection.offset);
+    const offset = collection.offset + collectionEOF + INT_SIZE;
+
+    await writeData(target.fd, offset, recordLengthBuffer, INT_SIZE);
+    await writeData(target.fd, offset + INT_SIZE, data, data.length);
+
+    const newEOF = collectionEOF + INT_SIZE + data.length;
+    const collectionLengthBuffer = Buffer.alloc(INT_SIZE);
+    collectionLengthBuffer.writeUInt32LE(newEOF, 0);
+    await writeData(target.fd, collection.offset, collectionLengthBuffer, INT_SIZE);
+}
+
+async function copyLiveRecords(source: BinManager, target: BinManager, collectionName: string, sourceOffset: number, collectionEOF: number) {
+    let readCursor = sourceOffset + INT_SIZE;
+    const endOffset = readCursor + collectionEOF;
+
+    while (readCursor < endOffset) {
+        const recordLengthBuffer = await readData(source.fd, readCursor, INT_SIZE);
+        const recordLength = recordLengthBuffer.readUInt32LE(0);
+        readCursor += INT_SIZE;
+
+        const data = await readData(source.fd, readCursor, recordLength);
+        readCursor += recordLength;
+
+        if (isRemovedRecord(data)) continue;
+
+        await appendRawRecord(target, collectionName, recordLengthBuffer, data);
+    }
+}
 
 export async function optimize(cmp: BinManager) {
     await _log(3, "Starting database optimization");
-    const collections = cmp.meta.collections;
+    const collections = [...cmp.meta.collections];
 
-    const allData = new Map<string, Buffer>();
-    for (const { name, offset } of collections) {
-        await _log(6, "Reading collection for optimization:", name);
-        const len = await readData(cmp.fd, offset, 4);
-        const data = await readData(cmp.fd, offset + 4, len.readInt32LE(0));
-        allData.set(name, data);
+    const tmpPath = `${cmp.path}.tmp`;
+    await _log(6, "Removing stale optimization temp file:", tmpPath);
+    await rm(tmpPath, { force: true });
+
+    const tmpMgr = new BinManager(tmpPath, cmp.options);
+    let tmpOpened = false;
+    let originalClosed = false;
+
+    try {
+        await tmpMgr.init();
+        tmpOpened = true;
+
+        const lengthBuffer = Buffer.alloc(INT_SIZE);
+        for (const { name, offset } of collections) {
+            await _log(6, "Optimizing collection:", name);
+            const collectionEOF = await readCollectionEof(cmp.fd, offset);
+
+            const collectionMeta = await ensureCollection(tmpMgr, name, 0, false);
+            lengthBuffer.writeUInt32LE(0, 0);
+            await writeData(tmpMgr.fd, collectionMeta.offset, lengthBuffer, INT_SIZE);
+            await copyLiveRecords(cmp, tmpMgr, name, offset, collectionEOF);
+        }
+
+        await saveHeaderAndPayload(tmpMgr);
+        await tmpMgr.close();
+        tmpOpened = false;
+
+        await _log(5, "Closing original file for optimization");
+        await cmp.close();
+        originalClosed = true;
+
+        await _log(5, "Replacing original database with optimized temp file");
+        await rename(tmpPath, cmp.path);
+        await cmp.init();
+        originalClosed = false;
+    } catch (err) {
+        if (tmpOpened) await tmpMgr.close();
+        await rm(tmpPath, { force: true });
+        if (originalClosed) await cmp.init();
+        throw err;
     }
 
-    await _log(5, "Closing file for optimization");
-    await cmp.close();
-    await _log(6, "Deleting old database file for optimization");
-    await unlink(cmp.path);
-    await new Promise(resolve => setTimeout(resolve, 100));
-    await _log(5, "Re-opening database file for optimization");
-    await cmp.init();
-
-    let offset = roundUpCapacity(cmp.meta, cmp.meta.payloadLength + HEADER_SIZE) + cmp.meta.blockSize;
-    const lengthBuffer = Buffer.alloc(INT_SIZE);
-    for (const [collection, data] of allData) {
-        await _log(6, "Writing optimized collection:", collection);
-        const len = roundUpCapacity(cmp.meta, data.length + INT_SIZE);
-        lengthBuffer.writeInt32LE(data.length, 0);
-        await writeData(cmp.fd, offset, lengthBuffer, INT_SIZE);
-        await writeData(cmp.fd, offset + INT_SIZE, data, len);
-        cmp.meta.collections.push({
-            name: collection,
-            offset,
-            capacity: len
-        });
-        offset += len;
-    }
-
-    await saveHeaderAndPayload(cmp);
     await _log(3, "Database optimization complete");
 }
